2952d0d8232dadf2f43fd67568f4dcda119a1731
MohamedMamdouh-98 <miido96@gmail.com>
2022-06-05 16:48:08 +0200

    .

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import './Intro.css';
+import Github from '../../images/github.png';
+
+
 
 function Intro() {
     return (
         <div className="intro">
             <div className="i-left">
                 <div className="i-name">
-                    <h1>hy! i am</h1>
-                    <span>mohamed</span><br/>
+                    <span>hy! i am</span>
+                    <span>mohamed mamdouh</span>
                     <span>front end developer with heigh level of experience in web design</span>
                 </div>
+                <button className="button i-button">hire me</button>
+                <div className="i-icons">
+                    <img src={Github} alt=""/>
+                </div>
+            </div>
+            <div className="i-right">
+            xxxxxxxxxxxxxx
             </div>
         </div>
     )
