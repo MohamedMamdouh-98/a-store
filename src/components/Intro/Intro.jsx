1b921fd4800eea5508385cc850f7922d0faee69d
MohamedMamdouh-98 <miido96@gmail.com>
2022-06-06 13:00:36 +0200

    Header part completed

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import './Intro.css';
 import Github from '../../images/github.png';
+import LinkedIn from '../../images/linkedin.png';
+import Instgram from '../../images/instagram.png';
+import Vector1 from '../../images/Vector1.png';
+import Vector2 from '../../images/Vector2.png';
+import boy from '../../images/boy.png';
+import thumbup from '../../images/thumbup.png';
+import Crown from '../../images/crown.png';
+import glassesimoji from '../../images/glassesimoji.png';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+
 
 
 
@@ -16,10 +26,30 @@ function Intro() {
                 <button className="button i-button">hire me</button>
                 <div className="i-icons">
                     <img src={Github} alt=""/>
+                    <img src={LinkedIn} alt=""/>
+                    <img src={Instgram} alt=""/>
                 </div>
             </div>
             <div className="i-right">
-            xxxxxxxxxxxxxx
+                <img src={Vector1} alt=""/>
+                <img src={Vector2} alt=""/>
+                <img src={boy} alt=""/>
+                <img src={glassesimoji} alt=""/>
+                <div style={{top: '2%', left: '55%'}}>
+                    <FloatingDiv img={Crown} txt1='web' txt2='developer'/>
+                </div>
+                <div style={{top: '380px', left: '-44px'}}>
+                <FloatingDiv img={thumbup} txt1='best design' txt2='amard'/>
+            </div>
+            <div className="blur" style={{background: 'rgb(238 210 255)'}}></div>
+            <div className="blur" style={{
+                background: '#c1f5ff', 
+                top: '17rem',
+                width: '21rem',
+                heigh: '11rem',
+                left: '-9rem'
+            }}>
+            </div>
             </div>
         </div>
     )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,7 +19,7 @@ function NavBar() {
                     </ul>
                 </div>
                 <button className="button n-button">
-                    contact us
+                    contact
                 </button>
             </div>
         </div>
