a6649dad6f10176cd32b7bddf85f83c5ca30ced7
MohamedMamdouh-98 <miido96@gmail.com>
2022-06-06 17:58:06 +0200

    The percentages section has been completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import NavBar from './components/NavBar';
 import './App.css';
 import Intro from './components/Intro/Intro';
 import Services from './components/Services/Services';
+import Experiments from './components/Experiments/Experiments';
+import Works from './components/Works/Works';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <NavBar />
       <Intro/>
       <Services/>
+      <Experiments/>
+      <Works/>
     </div>
   );
 }
